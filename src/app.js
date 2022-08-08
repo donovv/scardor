183514210abea849ca8b7fc6fe5e9f183ffdd7a1
donovv <donov14@gmail.com>
2022-08-07 22:03:46 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const mongoose = require('./mongoose');
 const app = express(feathers());
 
 const corsOptions = {
-  origin: 'https://dono702.com/',
+  origin: '*',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
 }
